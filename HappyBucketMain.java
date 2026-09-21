9f4f9faca95ed15133c0dd0840c4002116779bc2
DontCallMeTwinky <dontcallmetwinky@hotmail.com>
2026-09-21 14:45:32 +0100

    first commit

---
+++ HappyBucketMain.java
@@ -0,0 +1,10 @@
+public class HappyBucketMain {
+    private int i = 0;
+    public static void main(String[] args) {
+        HappyBucketMain happyBucket = new HappyBucketMain();
+        happyBucket.i = 5;
+        System.out.println("\nwelcome to the happy bucket!!!!11!!1\n");
+        System.out.println("yuo have THIS many days to live mort: " + happyBucket.i);
+        System.out.println("\n......unless i get my cupcakes.......\n");
+     }
+}
